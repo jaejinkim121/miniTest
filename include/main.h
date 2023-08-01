714781260e5391832bcde3281577f90473239e29
jaejinkim121 <gisgen9@snu.ac.kr>
2023-08-01 11:48:26 +0900

    Complete initial Skeleton

---
+++ include/main.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <Windows.h>
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include "fastech/MOTION_DEFINE.h"
+#include "fastech/ReturnCodes_Define.h"
+#include "fastech/FAS_EziMOTIONPlusE.h"
+
+class StepperMotor
+{
+
+private:
+    BYTE lastIP;
+    int id;
+    DWORD dwAxisStatus;
+    int mm_to_pulse = 1000;
+
+public:
+    StepperMotor(BYTE lastIP, int id);
+    ~StepperMotor();
+
+	void clearAlarm(int iBdID);
+	void setMotorParameter(int iBdID, int ppr, int maxSpeed);
+	int servoEnable(int iBdID);
+	int findZeroPosition();
+    int moveSingleAxis_mm(int id, double posAbs, double period);
+    int isMotioning(int id);
+
+
+};
+
+int PPR = 10;
+int MaxSpeed = 40000;
+BYTE lastIP = 18;
+int id = 2;
---
+++ src/main.cpp
@@ -0,0 +1,73 @@
+#include "../include/main.h"
+
+StepperMotor::StepperMotor(BYTE lastIP, int id){
+    while(FAS_ConnectTCP(192,168,0,lastIP, id) == FALSE){
+        std::cout << "Connection Failed" << std::endl;
+        Sleep(1000);
+    }
+
+    std::cout << "Successfully Connected" << std::endl;
+    this->lastIP = lastIP;
+    this->id= id;
+
+}
+
+StepperMotor::~StepperMotor(){
+    FAS_Close(id);
+}
+
+int StepperMotor::clearAlarm(int id){
+    return FAS_ServoAlarmReset(id);
+}
+
+int StepperMotor::setMotorParameter(int id, int ppr, int maxspeed){
+    FAS_SetParameter(iBdID, 0, ppr);
+	FAS_SetParameter(iBdID, 1, maxSpeed);
+
+    clearAlarm(id);
+    return;
+}
+
+int StepperMotor::servoEnable(int id){
+    FAS_ServoEnbale(id, TRUE);
+}
+
+int StepperMotor::findZeroPosition(){
+    
+}
+
+int StepperMotor::moveSingleAxis_mm(int id, double posAbs, double period){
+    long pulseAbs = posAbs * mm_to_pulse;	// 1rev : 1mm -> 1/1000 rev : 1 micron
+	int velocity = abs(pulseAbs	/ period);
+
+    FAS_MoveSingleAxisAbsPos(id, pulseAbs, velocity);
+
+}
+
+int StepperMotor::isMotioning(int id){
+    EZISERVO_AXISSTATUS stAxisStatus;
+    int nRtn;
+    do{
+        Sleep(1);
+        int nRtn = FAS_GetAxisStatus(id, &dwAxisStatus);
+        stAxisStatus.dwValue = dwAxisStatus;
+    }
+    while(stAxisStatus.FFLAG_MOTIONING);
+}
+
+
+int main(){
+    StepperMotor *motor1 = new StepperMotor(lastIP, id);
+
+    motor1->setMotorParameter(id, PPR, MaxSpeed);
+    motor1->clearAlarm(id);
+    motor1->servoEnable(id);
+
+    for (int i = 0; i< 10 ; i++){
+        motor1->moveSingleAxis_mm(id, 10000, 1);
+        motor1->isMotioning(id);
+
+        motor1->moveSingleAxis_mm(id, 0, 1);
+        motor1->isMotioning(id);
+    }
+}
